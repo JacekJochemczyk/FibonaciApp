e625d54b353c1738dffe676c1f85789ccbcf4225
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-11-03 20:39:25 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FibonaciApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstFibonacci = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(381, 106);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "wyswietl";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstFibonacci
+            // 
+            this.lstFibonacci.FormattingEnabled = true;
+            this.lstFibonacci.ItemHeight = 16;
+            this.lstFibonacci.Location = new System.Drawing.Point(366, 184);
+            this.lstFibonacci.Name = "lstFibonacci";
+            this.lstFibonacci.Size = new System.Drawing.Size(110, 180);
+            this.lstFibonacci.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kliknij wyswietl aby wyswietlic 10 pierwszych liczb Fibonacciego";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstFibonacci);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstFibonacci;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonaciApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        // Obsługa zdarzenia kliknięcia przycisku
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Czyść listę przed każdym obliczeniem
+            lstFibonacci.Items.Clear();
+
+            // Oblicz i dodaj do listy pierwsze 10 liczb Fibonacciego
+            for (int i = 0; i < 10; i++)
+            {
+                int fibNumber = Fibonacci(i);
+                lstFibonacci.Items.Add($"F({i}) = {fibNumber}");
+            }
+        }
+    }
+}
+
